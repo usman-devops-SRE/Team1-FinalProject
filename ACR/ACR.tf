c96f701ede2aa8a11e673fde04062d281eb459d1
Brad McKenzie <mckzcoast@yahoo.com>
2022-03-08 15:55:26 -0500

    ACR

---
+++ ACR/ACR.tf
@@ -0,0 +1,22 @@
+resource "azurerm_resource_group" "ACR" {
+  name     = var.ACR
+  location = var.location
+}
+
+resource "azurerm_container_registry" "ARC1" {
+  name                = azurerm_container_registry.ARC1.name
+  resource_group_name = azurerm_resource_group.ACR.name
+  location            = azurerm_resource_group.ACR.location
+  sku                 = "Premium"
+  admin_enabled       = false
+  georeplications {
+    location                = "eastus"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+  georeplications {
+    location                = "westeurope"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+}
