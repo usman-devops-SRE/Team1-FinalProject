c7d4ebde39debbbe8f6615f5dfdbcf69b0972df3
MikeWolski <wolski17@gmail.com>
2022-03-09 14:55:32 -0500

    updated

--- CosmosDB/terraform.tfvars
+++
@@ -1,6 +0,0 @@
-# Variables
-resource_group_name        = "cosmos_db_resource_group"
-resource_group_location    = "centralus"
-failover_location          = "westeurope"
-
-
---
+++ main.tf
@@ -0,0 +1,11 @@
+module "cosmos_db" {
+    source = "./modules/Cosmos/"
+    location = azurerm_resource_group.rg.location
+    rg_name = azurerm_resource_group.rg.name
+    failover_location = "westeurope"
+}
+
+resource "azurerm_resource_group" "rg" {
+    name = "cosmos_db_rg"
+    location = "eastus"
+}
