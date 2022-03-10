e4747d9ad7ac846332dd77394f318fa0f7021f72
MikeWolski <wolski17@gmail.com>
2022-03-10 09:12:09 -0500

    fixed arguments

--- Cosmos.tf
+++ Cosmos.tf
@@ -1,7 +1,7 @@
 module "cosmos_db" {
   source               = "./modules/Cosmos/"
-  location             = azurerm_resource_group.db_rg.location
-  rg_name              = azurerm_resource_group.db_rg.name
+  db_location             = azurerm_resource_group.db_rg.location
+  db_rg_name              = azurerm_resource_group.db_rg.name
   db_failover_location = "westeurope"
   subnet3_id           = module.vnet1.subnet3_id
 }
