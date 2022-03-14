23ef9b69402abc2fcc5e5bba9859e6f0aeac4a1a
Ubhatti94 <uzzi.bhti@gmail.com>
2022-03-14 15:59:25 -0400

    removed unnecessary variables

--- modules/bastion_vm/bastion_vm_variables.tf
+++ modules/bastion_vm/bastion_vm_variables.tf
@@ -11,19 +11,8 @@ variable "rg_name"{
     type = string
 }
 
-variable "account_uri"{
-  type = string
-}
-
 
 #######################
-variable "nic_name" {
-  type = string
-}
-
-variable "storage_account_type" {
-  type = string
-}
 
 variable "admin_username" {
   type = string
@@ -31,51 +20,18 @@ variable "admin_username" {
 variable "admin_password"{
     type=string
 }
-
-variable "vm_size" {
-  type = string
-}
-
 variable "vm_name" {
   type = string
 }
 
-
-variable "caching"{
-  type=string
-}#"ReadWrite"
-variable "publisher"{
-  type=string
-}#"Canonical"
-variable "offer"{
-  type=string
-}#"UbuntuServer"
-variable "image_sku"{
-  #"18.04-LTS"
-  type=string
-}
 variable "use_ssh_keys"{
   type=string
 }
 ######################### nic variables.tf
-variable "nic_ipconf_name"{
-  #"internal"
-  type=string
-}
-variable "nic_ipconf_subnetid"{
-  type=string
-  #azurerm_subnet.subnet.id
-}
-variable "nic_ipconf_pip_addr"{
-  #azurerm_public_ip.pip.id
-  type=string
-}
-variable "nic_ipconf_priv_alloc"{
-  type=string
-}
-variable "custom_data"{
-  type=string
-}
+
 variable "comp_name"{
+  type=string
+}
+variable "vm_size"{
   type=string
 }
