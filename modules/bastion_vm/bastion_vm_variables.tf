b9ad35498dc976c651fb84987aa87fe5d1fbe735
Ubhatti94 <uzzi.bhti@gmail.com>
2022-03-14 15:52:35 -0400

    duplicate variable error fixed

--- modules/bastion_vm/bastion_vm_variables.tf
+++ modules/bastion_vm/bastion_vm_variables.tf
@@ -14,12 +14,8 @@ variable "rg_name"{
 variable "account_uri"{
   type = string
 }
-variable "rg_name" {
-  type = string
-}
-variable "location" {
-  type = string
-}
+
+
 #######################
 variable "nic_name" {
   type = string
