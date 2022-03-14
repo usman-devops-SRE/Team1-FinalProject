5e9d68f9f39b93518212a3c2f7668f6218573f7f
Ubhatti94 <uzzi.bhti@gmail.com>
2022-03-14 16:20:21 -0400

    disabled public ip on vm's nic

--- modules/bastion_vm/bastion_vm.tf
+++ modules/bastion_vm/bastion_vm.tf
@@ -65,7 +65,7 @@ resource "azurerm_network_interface" "nic" {
   ip_configuration {
     name                          = "internal" #"${var.nic_ipconf_name}"#
     subnet_id                     = azurerm_subnet.bastion_subnet.id #"${var.nic_ipconf_subnetid}"#azurerm_subnet.subnet.id
-    public_ip_address_id          = azurerm_public_ip.bastion_public_ip.id #"${var.nic_ipconf_pip_addr}"#azurerm_public_ip.pip.id
+    public_ip_address_id          = ""#azurerm_public_ip.bastion_public_ip.id #"${var.nic_ipconf_pip_addr}"#azurerm_public_ip.pip.id
     private_ip_address_allocation = "Dynamic" #"${var.nic_ipconf_priv_alloc}"#
   }
 }
