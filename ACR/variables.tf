567fe8f5995e485aa7b604623db74b4aba29a22c
tylerkain <68011581+tylerkain@users.noreply.github.com>
2022-03-22 08:46:34 -0500

    Update variables.tf

--- ACR/variables.tf
+++ ACR/variables.tf
@@ -1,6 +1,7 @@
 variable "Team1P3ACR" {
   type        = string
   description = "Container registery name "
+  default = "Team1P3ACR"
 }
 variable "resource_group_name" {
   type = string
@@ -14,34 +15,5 @@ variable "ACIrg" {
 variable "location" {
   type        = string
   description = "Azure Resource Region Location"
-}
- ####Kubernetes variables
- variable "k8s_rg1" {
-   type = string
-   description = "resource group for kubernetes"
- }
- variable "nodes_rg" {
-   type = string
- }
- variable "ueNodesCluster" {
-  type = string
-  description = "cluster name"
-}
-variable "node_count" {
-  type = number
-  description = "number of nodes"
-}
-
-variable "node_pool_name" {
-  type = string
-  description = "name of the node pool"
-}
-
-variable "vm_size" {
-  type = string
-  description = "vm size for node pool"
-}
-variable "location2" {
-  type = string
-  description = "Second rg location"
+  default = "East Us"
 }
