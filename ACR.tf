0f5ba24ff45c4f68cad44d5e96625e9a17fd4136
tylerkain <68011581+tylerkain@users.noreply.github.com>
2022-03-22 13:50:33 -0500

    Update ACR.tf

--- ACR.tf
+++ ACR.tf
@@ -40,7 +40,7 @@ resource "azurerm_container_registry_task" "JLT1" {
   docker_step {
     dockerfile_path      = "Dockerfile"
     context_path         = "https://github.com/tylerkain/Team1-FinalProject/tree/finaldocker"
-    context_access_token = ""
+    context_access_token = "GitHUB"
     image_names          = ["bradmcken/team_1_final_project:latest", "bradmcken/team_1_final_project:apilatest", "bradmcken/team_1_final_project:dblatest"]
   }
 }
