d556c43d5bfd69ed100fc0c5ab9de38b2990a378
tylerkain <68011581+tylerkain@users.noreply.github.com>
2022-03-22 08:56:09 -0500

    Update variables.tf

--- ACR/variables.tf
+++ ACR/variables.tf
@@ -6,12 +6,9 @@ variable "Team1P3ACR" {
 variable "resource_group_name" {
   type = string
   description = "resource group name for ACR"
-}
-variable "ACIrg" {
-  type = string
   default = "acr-team-3rg"
-  description = "resouce group varsname"
 }
+
 variable "location" {
   type        = string
   description = "Azure Resource Region Location"
