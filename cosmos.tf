e5096aa6c489135bda8b7fa8cb01c27b2a165225
MikeWolski <wolski17@gmail.com>
2022-03-23 17:56:10 -0400

    changed resource group

--- cosmos.tf
+++ cosmos.tf
@@ -1,7 +1,7 @@
 module "cosmos_db" {
   source                = "./modules/cosmos/"
-  db_location           = azurerm_resource_group.k8s_rg1.location
-  db_rg_name            = azurerm_resource_group.k8s_rg1.name
+  db_location           = azurerm_resource_group.nodes_rg.location
+  db_rg_name            = azurerm_resource_group.nodes_rg.name
   db_failover_location  = "westeurope"
   db_account_name       = "cosmosdb-team1-project3"
   #subnet3_id           = module.vnet1.subnet3_id
