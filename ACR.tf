d7a45554011e9976168b5f83eab17870ecf7d62f
tylerkain <68011581+tylerkain@users.noreply.github.com>
2022-03-23 16:40:52 -0500

    Create ACR.tf

---
+++ ACR.tf
@@ -0,0 +1,27 @@
+data "azurerm_client_config" "current" {}
+
+####### Resource Group for The Azure Registar conatiner & Azure Container Instance
+resource "azurerm_resource_group" "ACI" {
+  name     = var.resource_group_name
+  location = var.location 
+}
+########### Azure Container Registery 
+resource "azurerm_container_registry" "T1RC" {
+  name                = var.Team1P3ACR
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = "Premium"
+  identity {
+    type = "SystemAssigned"
+  }
+  georeplications {
+    location                = "Central US"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+  georeplications {
+    location                = "westeurope"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+}
