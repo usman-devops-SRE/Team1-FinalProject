cd097365f057c36797ee9c12ce79bf6f5a58f624
tylerkain <68011581+tylerkain@users.noreply.github.com>
2022-03-23 07:42:14 -0500

    Update ACR.tf

--- ACR.tf
+++ ACR.tf
@@ -1,3 +1,6 @@
+############# data
+data "azurerm_client_config" "current" {}
+
 ####### Resource Group for The Azure Registar conatiner & Azure Container Instance
 resource "azurerm_resource_group" "ACI" {
   name     = var.resource_group_name
@@ -42,5 +45,35 @@ resource "azurerm_container_registry_task" "JLT1" {
     context_path         = "https://github.com/tylerkain/Team1-FinalProject/tree/finaldocker"
     context_access_token = "Github"
     image_names          = ["bradmcken/team_1_final_project:latest", "bradmcken/team_1_final_project:apilatest", "bradmcken/team_1_final_project:dblatest"]
+  }
+}
+resource "azurerm_key_vault" "vault" {
+  name                        = var.Team1vault
+  location                    = var.location
+  resource_group_name         = var.ACIrg
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    certificate_permissions = [ "Backup", "Create", "Delete", "DeleteIssuers", "Get", "GetIssuers", "Import", "List", "ListIssuers", "ManageContacts", "ManageIssuers", "Purge", "Recover", "Restore", "SetIssuers", "Update"  ]
+
+    key_permissions = [
+      "Get", "Backup", "Create", "Decrypt","Delete", "Encrypt", "Import", "List", "Purge", "Recover", "Restore", "Sign", "UnwrapKey", "Update", "Verify", "WrapKey"
+    ]
+
+    secret_permissions = [
+      "Get","Backup" , "Delete", "List", "Purge", "Recover", "Restore", "Set"
+    ]
+
+    storage_permissions = [
+      "Get", "Backup", "Delete", "DeleteSAS", "GetSAS", "List", "ListSAS", "Purge", "Recover", "RegenerateKey", "Restore", "Set", "SetSAS", "Update"
+    ]
   }
 }
