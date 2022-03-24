0058971b0a75f58d0c61eae3f281a8592b082e51
MikeWolski <wolski17@gmail.com>
2022-03-24 09:34:36 -0400

    fixed error

--- cosmos.tf
+++ cosmos.tf
@@ -1,7 +1,7 @@
 module "cosmos_db" {
   source                = "./modules/cosmos/"
-  db_location           = azurerm_resource_group.nodes_rg.location
-  db_rg_name            = azurerm_resource_group.nodes_rg.name
+  db_location           = azurerm_kubernetes_cluster.k8cluster_1.location
+  db_rg_name            = azurerm_kubernetes_cluster.k8cluster_1.resource_group_name
   db_failover_location  = "westeurope"
   db_account_name       = "cosmosdb-team1-project3"
   #subnet3_id           = module.vnet1.subnet3_id
